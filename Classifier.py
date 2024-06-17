f8c8aaad56da3378a32d897a2c633b6fe2335e68
Phan Tran Minh Dat <minhdatphantran2205@gmail.com>
2024-06-17 22:52:11 +0700

    Update: 3-class discriminant func

--- Classifier.py
+++ Classifier.py
@@ -78,11 +78,11 @@ class TwoClassesOneOneClassifier(BaseClassifier):
             for i, test in enumerate(test_data):
                 class_rank = [0,0,0]
                 for (first, second) in class_pairs:
-                    idx = np.where((test_target == first) | (test_target == second))
-                    # tempX = test_data[idx]
-                    tempT = test_target[idx]
-                    tempT[tempT == first] = 0
-                    tempT[tempT == second] = 1
+                    # idx = np.where((test_target == first) | (test_target == second))
+                    # # tempX = test_data[idx]
+                    # tempT = test_target[idx]
+                    # tempT[tempT == first] = 0
+                    # tempT[tempT == second] = 1
                         
                     if self.predict(np.append(test,1), (first, second)) == 0:
                         class_rank[first] += 1
@@ -145,11 +145,11 @@ class TwoClassesOneRestClassifier(BaseClassifier):
             for i, test in enumerate(test_data):
                 class_rank = [0,0,0]
                 for first in range(3):
-                    idx = np.where((test_target == first) | (test_target != first))
-                    tempT = test_target[idx]
-                    tempCond = (tempT != first)
-                    tempT[tempT == first] = 0
-                    tempT[tempCond] = 1
+                    # idx = np.where((test_target == first) | (test_target != first))
+                    # tempT = test_target[idx]
+                    # tempCond = (tempT != first)
+                    # tempT[tempT == first] = 0
+                    # tempT[tempCond] = 1
                     # print("TempT",test_target)
                         
                     if self.predict(np.append(test,1), first) == 0:
@@ -159,6 +159,75 @@ class TwoClassesOneRestClassifier(BaseClassifier):
                         for j in range(len(class_rank)):
                             if j != first:
                                 class_rank[j] += 1
+                pred = class_rank.index(max(class_rank))
+                # print("Rank:",class_rank)
+                # print(test_target)
+                # print("Test:",pred,i,test_target[i])
+                
+                if pred == test_target[i]:
+                    acc += 1
+                
+            acc_by_folds.append(acc / len(test_data))
+
+        return acc_by_folds
+    
+class ThreeClassesClassifier(BaseClassifier):
+    def __init__(self, data, target):
+        super().__init__(data,target)
+    
+    def __str__(self):
+        return f"ThreeClassesClassifier(numClasses = {len(np.unique(self.target))})"
+    
+    def fit(self, train_data, train_target):
+        for first in range(3):
+            idx = np.where((train_target >= 0))
+            # print(idx)
+            tempX = train_data[idx]
+            # tempT = train_target[idx]
+            tempT = np.zeros((len(train_target[idx]), 3))
+            tempT[np.arange(len(train_target[idx])), train_target[idx]] = 1
+            
+            ones_column = np.ones((tempX.shape[0], 1))
+            tempX = np.hstack((tempX, ones_column))
+            # print("===",tempT)
+            # tempT[tempT == first] = -1
+            # tempT[tempT != -1] = 1
+            # print("Train target:",train_target)
+            # print("TempT",tempT)
+            self._w[first] = np.dot(np.linalg.pinv(tempX),tempT)
+        # print("***",self._w)
+        return self._w
+
+    def predict(self, test, pair_id):
+        res = np.dot(np.transpose(self._w[pair_id]),np.array(test)).tolist()
+        # print(res)
+        return res.index(max(res))
+    
+    def test(self, k):
+        kf = KFold(n_splits=k, shuffle=True, random_state=50)
+        acc_by_folds = []
+        
+        for train_index, test_index in kf.split(self.data):
+            train_data, test_data = self.data[train_index], self.data[test_index]
+            train_target, test_target = self.target[train_index], self.target[test_index]
+            
+            # class_pairs = list(combinations(np.unique(test_target), 2))
+            self.fit(train_data,train_target)
+            # print("W:",self._w)
+            acc = 0
+            # print(test_target)
+            for i, test in enumerate(test_data):
+                class_rank = [0,0,0]
+                for first in range(3):
+                    # idx = np.where((test_target == first) | (test_target != first))
+                    # tempT = test_target[idx]
+                    # tempCond = (tempT != first)
+                    # tempT[tempT == first] = 0
+                    # tempT[tempCond] = 1
+                    # print("TempT",test_target)
+                        
+                    class_rank[self.predict(np.append(test,1), first)] += 1
+                # print(class_rank)
                 pred = class_rank.index(max(class_rank))
                 # print("Rank:",class_rank)
                 # print(test_target)
---
+++ main.py
@@ -0,0 +1,12 @@
+from sklearn import datasets
+import Classifier as clf
+
+DATA_PER_CLASS = 50
+
+iris = datasets.load_iris()
+data = iris.data
+target = iris.target
+
+base = clf.ThreeClassesClassifier(data,target)
+
+print(base.test(5))
